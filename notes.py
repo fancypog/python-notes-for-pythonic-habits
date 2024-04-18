fcab0278d7bbc5e5172d873619533937711ad4b0
Frances <135648147+fancypog@users.noreply.github.com>
2024-04-18 19:32:12 +0100

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,14 @@
+# 1. snake case
+# python recommends snake case where lowercase words are connected by underscore
+
+def get_maximum_number:
+
+
+# 2. use f strings 
+
+
+# 3. key word 'is' 
+
+
+
+
